29292e0bcf9bffaefda0d7479111dd0b41cd3c84
Balazs Varga <warnyul@gmail.com>
2018-02-10 20:10:31 +0100

    Use xcodebuild instead of xctool for builds on travis

---
+++ TravisTestAppTests/TravisTestAppTests.m
@@ -0,0 +1,39 @@
+//
+//  TravisTestAppTests.m
+//  TravisTestAppTests
+//
+//  Created by Balazs Varga on 2018. 02. 10..
+//  Copyright © 2018. Balazs Varga. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TravisTestAppTests : XCTestCase
+
+@end
+
+@implementation TravisTestAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
